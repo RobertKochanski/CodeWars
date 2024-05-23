8e450c47ecd811dfb714c0516d80836d78ca4751
RobertKochanski <kochanskirobert1999@gmail.com>
2024-05-23 12:53:34 +0200

    kyu5 kata

---
+++ Product_of_consecutive_Fib_numbers/Product_of_consecutive_Fib_numbers/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ProdFib
+{
+    public static ulong[] productFib(ulong prod)
+    {
+        List<ulong> fibonacci = new List<ulong>() { 0, 1 };
+        int n = 1;
+
+        while (fibonacci[n] * fibonacci[n - 1] < prod)
+        {
+            fibonacci.Add(fibonacci[n] + fibonacci[n - 1]);
+            n++;
+        }
+
+        ulong tf = (ulong)((fibonacci[n - 1] * fibonacci[n] == prod) ? 1 : 0);
+
+        return new ulong[] { (ulong)fibonacci[n - 1], (ulong)fibonacci[n], tf };
+    }
+}
---
+++ Product_of_consecutive_Fib_numbers/Product_of_consecutive_Fib_numbers/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class ProdFibTests
+{
+
+    [Test]
+    public void Test1()
+    {
+        ulong[] r = new ulong[] { 55, 89, 1 };
+        Assert.AreEqual(r, ProdFib.productFib(4895));
+    }
+}
