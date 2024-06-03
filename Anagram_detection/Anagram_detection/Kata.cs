a6c51812ea81841b4a3305edbbdc5f7283d5e048
RobertKochanski <kochanskirobert1999@gmail.com>
2024-06-03 14:42:51 +0200

    Anagram detection kata

---
+++ Anagram_detection/Anagram_detection/Kata.cs
@@ -0,0 +1,35 @@
+﻿namespace Anagram_detection
+{
+    public class Kata
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            test = test.ToLower();
+            original = original.ToLower();
+
+            if (test.Length != original.Length)
+            {
+                return false;
+            }
+
+            foreach (char c in test)
+            {
+                if (original.Contains(c))
+                {
+                    original = original.Remove(original.IndexOf(c), 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (original.Length != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Anagram_detection/Anagram_detection/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Anagram_detection;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCase("foefet", "toffee", ExpectedResult = true)]
+    [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+    [TestCase("Twoo", "Woot", ExpectedResult = true)]
+    [TestCase("apple", "pale", ExpectedResult = false)]
+    public static bool FixedTest(string test, string original)
+    {
+        return Kata.IsAnagram(test, original);
+    }
+}
